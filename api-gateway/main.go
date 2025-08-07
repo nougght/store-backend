2e27a12eed2d6b4230b12c5cffeee968e6647d2b
nougght <muhamedmakusev@gmail.com>
2025-08-07 21:39:28 +0300

    api-gateway queries

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -24,6 +24,15 @@ func main() {
 	}))
 
 	r.RedirectTrailingSlash = false // Отключаем авторедиректы
+	r.POST("/user/check/:email_or_phone", authServiceURL)
+	r.GET("/user/:user_id", authServiceURL)
+	r.GET("/user/:user_id/session", authServiceURL)
+	r.POST("user/logout/:user_id", authServiceURL)
+
+	r.POST("auth/login", authServiceURL)
+	r.POST("auth/register", authServiceURL)
+	r.POST("auth/code/send", authServiceURL)
+	r.POST("auth/code/verify", authServiceURL)
 
 	r.GET("/cart/:user_id", reverseProxy(cartServiceURL))
 	r.GET("/cart/items/:cart_id", reverseProxy(cartServiceURL))
