f565cfce23c2070b7b5cc7cc02171f90e29a7515
nougght <muhamedmakusev@gmail.com>
2025-08-18 23:32:27 +0300

    fx

--- product-service/internal/handlers/products_handler.go
+++ product-service/internal/handlers/products_handler.go
@@ -39,7 +39,7 @@ func (h *ProductsHandler) GetProducts(c *gin.Context) {
 			page := c.Query("page")
 			limit := c.Query("limit")
 			var filters map[string]string
-			js.NewDecoder(c.Request.Body).Decode(&filters)
+			js.NewDecoder(c.Query("filters")).Decode(&filters)
 			sort := strings.Split(filters["sort"], "_")
 			category := filters["category"]
 
