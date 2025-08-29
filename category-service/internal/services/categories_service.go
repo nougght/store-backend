99e5aba771435a6fe4dd1c99ee62e7be39c42b92
nougght <muhamedmakusev@gmail.com>
2025-08-29 18:35:18 +0300

    fx

--- category-service/internal/services/categories_service.go
+++ category-service/internal/services/categories_service.go
@@ -10,23 +10,21 @@ type CategoriesService struct {
 	repo *repositories.CategoriesRepository
 }
 
-func NewCategoriesService (repo *repositories.CategoriesRepository) *CategoriesService  {
+func NewCategoriesService(repo *repositories.CategoriesRepository) *CategoriesService {
 	return &CategoriesService{repo: repo}
 }
 
-func (s *CategoriesService) CreateCategory(ctx context.Context, category models.Category) error {
+func (s *CategoriesService) CreateCategory(ctx context.Context, category *models.Category) error {
 	return s.repo.CreateCategory(ctx, &category)
 }
-
 
 func (s *CategoriesService) GetCategories(ctx context.Context) ([]models.Category, error) {
 	return s.repo.GetCategories(ctx)
 }
 
-func  (s *CategoriesService) UpdateCategory(ctx context.Context, category models.Category) error {
+func (s *CategoriesService) UpdateCategory(ctx context.Context, category models.Category) error {
 	return s.repo.UpdateCategory(ctx, &category)
 }
-
 
 func (s *CategoriesService) DeleteCategory(ctx context.Context, id string) error {
 	return s.repo.DeleteCategory(ctx, id)
